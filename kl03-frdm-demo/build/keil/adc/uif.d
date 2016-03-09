ed1535bb0af0b3ecb95bd10bc84d2e676529428a
wenxue <b51443@freescale.com>
2016-03-09 15:55:24 +0800

    add adc

---
+++ kl03-frdm-demo/build/keil/adc/uif.d
@@ -0,0 +1,12 @@
+.\uif.o: ..\..\..\src\common\uif.c
+.\uif.o: ..\..\..\src\common\common.h
+.\uif.o: ..\..\..\src\projects\platinum\isr.h
+.\uif.o: ..\..\..\src\cpu\arm_cm0.h
+.\uif.o: ..\..\..\src\platforms\freedom.h
+.\uif.o: ..\..\..\src\drivers\mcg\mcg_lite.h
+.\uif.o: ..\..\..\src\cpu\headers\MKL03Z4.h
+.\uif.o: C:\Freescale\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\uif.o: ..\..\..\src\common\assert.h
+.\uif.o: ..\..\..\src\common\io.h
+.\uif.o: ..\..\..\src\common\stdlib.h
+.\uif.o: ..\..\..\src\common\uif.h
